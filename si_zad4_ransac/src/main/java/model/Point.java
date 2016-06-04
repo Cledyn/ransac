b2e55afb2cad2ca05f31882412c6fa11f5d7698e
Cledyn <ssandra.kk@gmail.com>
2016-06-04 20:27:27 +0200

    Implement hashCode and equals methods

--- si_zad4_ransac/src/main/java/model/Point.java
+++ si_zad4_ransac/src/main/java/model/Point.java
@@ -1,5 +1,7 @@
 package model;
 
+import com.google.common.base.Objects;
+
 /**
  * Created by Sandra on 2016-06-04.
  */
@@ -7,11 +9,26 @@ public class Point {
 
     private double x;
     private double y;
-    private int [] features;
-    private Point point = null;
+    private int[] features;
+    private Point neighbour = null;
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) return false;
+        final Point other = (Point) obj;
+        return Objects.equal(this.x, other.x)
+                && Objects.equal(this.y, other.y)
+                && Objects.equal(this.neighbour, other.neighbour);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hashCode(
+                this.x, this.y, this.neighbour);
+
     }
 }
--- si_zad4_ransac/src/main/java/parser/FeaturesParser.java
+++ si_zad4_ransac/src/main/java/parser/FeaturesParser.java
@@ -12,18 +12,22 @@ import java.util.Scanner;
 public class FeaturesParser {
 
     private static final String SPACE_SEPARATOR = " ";
+    private static final int X_COORDINATE_INDEX_IN_LINE = 0;
+    private static final int Y_COORDINATE_INDEX_IN_LINE = 1;
+    private static final int FEATURE_START_INDEX = 6;
 
-    private void parseFeatures(String fileName){
+    private void parseFeatures(String fileName) {
         Scanner sc = new Scanner(fileName);
         int featuresNo = Integer.parseInt(sc.nextLine());
         int pointsNo = Integer.parseInt(sc.nextLine());
-        int [][] features = new int [featuresNo][pointsNo];
+        int[][] features = new int[featuresNo][pointsNo];
         List<Point> points = new ArrayList<Point>(8995);
         int lineIndex = 0;
-        while(sc.hasNext()){
+        while (sc.hasNext()) {
             String featuresForSinglePoint = sc.next();
-            String [] data = featuresForSinglePoint.split(SPACE_SEPARATOR);
-            double x = Double.parseDouble(data[0]);
+            String[] data = featuresForSinglePoint.split(SPACE_SEPARATOR);
+            double x = Double.parseDouble(data[X_COORDINATE_INDEX_IN_LINE]);
+            double y = Double.parseDouble(data[Y_COORDINATE_INDEX_IN_LINE]);
             lineIndex++;
 
         }
