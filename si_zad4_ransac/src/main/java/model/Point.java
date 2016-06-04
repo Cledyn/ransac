8365712ff8c73e478faac81ac22ee45f7d2829fa
Cledyn <ssandra.kk@gmail.com>
2016-06-04 20:18:03 +0200

    Added point class and initial code for feature parser

---
+++ si_zad4_ransac/src/main/java/model/Point.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by Sandra on 2016-06-04.
+ */
+public class Point {
+
+    private double x;
+    private double y;
+    private int [] features;
+    private Point point = null;
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
--- si_zad4_ransac/src/main/java/parser/FeaturesParser.java
+++ si_zad4_ransac/src/main/java/parser/FeaturesParser.java
@@ -1,7 +1,31 @@
 package parser;
 
+import model.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by Sandra on 2016-06-04.
  */
 public class FeaturesParser {
+
+    private static final String SPACE_SEPARATOR = " ";
+
+    private void parseFeatures(String fileName){
+        Scanner sc = new Scanner(fileName);
+        int featuresNo = Integer.parseInt(sc.nextLine());
+        int pointsNo = Integer.parseInt(sc.nextLine());
+        int [][] features = new int [featuresNo][pointsNo];
+        List<Point> points = new ArrayList<Point>(8995);
+        int lineIndex = 0;
+        while(sc.hasNext()){
+            String featuresForSinglePoint = sc.next();
+            String [] data = featuresForSinglePoint.split(SPACE_SEPARATOR);
+            double x = Double.parseDouble(data[0]);
+            lineIndex++;
+
+        }
+    }
 }
